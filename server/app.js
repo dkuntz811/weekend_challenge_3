64f8de98b790f8f5a7e98c1cf500ff5a59178790
Dave Kuntz <dkuntz811@gmail.com>
2016-08-15 08:40:26 -0500

    Monday AM

--- server/app.js
+++ server/app.js
@@ -3,10 +3,27 @@ var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 
+var calculator = [];
+
 app.set('port', process.env.PORT || 3000);
 
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.post('/calculator', function (req, res) {
+  console.log('request: ', req.body);
+	var calculator = req.body;
+	var numOne = calculator.num1;
+	var numTwo = calculator.num2;
+
+   calculator.push(req.body);
+	 res.sendStatus(201);
+});
+
+app.get('/calculator', function (req, res) {
+  res.send(calculator);
+});
 
 app.get('/*', function (req, res){
 	var file = req.params[0] || '/views/index.html';
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,45 @@
 $(document).ready(function(){
 	//console.log("I'm Ready");
+	   getNumbers();
+	$("#calcultor").on('click', function (){
+		event.preventDefault();
 
+	  var numbers = {};
+
+		$.each($("#calculator").serializeArray(), function (i, field) {
+		      numbers[field.name] = field.value;
+
+
+		    });
+
+				console.log('numbers submitted as', numbers);
+
+		$.ajax({
+			type: 'POST',
+			url: '/calculator',
+			data: numbers,
+			success: function (response) {
+				console.log('POST /calculator works');
+				getNumbers();
+			}
+		});
+
+		function getNumbers() {
+			$.ajax({
+				type: 'GET',
+				url: '/calculator',
+				success: function (calculator) {
+					$('#calculator').empty();
+					calculator.forEach(function (numbers) {
+						$('calculator').append('<div>' + number.num1 + number.num2 + '</div>');
+					});
+				},
+				error: function () {
+					console.log('Get /calculator did not work');
+				},
+			});
+		}
+
+
+	});
 });
